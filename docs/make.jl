806884a643d307dd13613f2fff72ec443f5f64cc
Lucas <luc.cammann@googlemail.com>
2021-09-27 01:30:05 +0200

    run doc

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,13 @@ push!(LOAD_PATH, "../src/");
 using Documenter, JuMPDiff
 
 makedocs(sitename="JuMPDiff",
+         format = Documenter.HTML(
+                            prettyurls = get(ENV, "CI", nothing) == "true"),
          authors = "Lucas Cammann",
-         pages = ["Introduction" => "index.md"]
+         doctest = true,
+         pages = ["Introduction" => "index.md",
+                  "Gradients" => "pages/gradients.md",
+                  "Jacobians" => "pages/jacobians.md",
+                  "Hessians" => "pages/hessians.md",
+                  "Geometric example" => "pages/geometric.md"]
 );
